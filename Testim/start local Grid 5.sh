8d93c11ed60ae1a6593ba351f96fae53a56809c1
Mihail <riba-kit17@mail.ru>
2017-03-04 23:48:27 +0300

    xterms instead of mate-terminal

--- Testim/start local Grid 5.sh
+++ Testim/start local Grid 5.sh
@@ -1,7 +1,7 @@
-mate-terminal -e "java -jar server.jar  -role hub"
+xterm -geometry 150x30 -bg blue -e java -jar server.jar -role hub &
 sleep 10
-mate-terminal -e "java -jar server.jar -role webdriver -hub http://localhost:4444/grid/register -port 5551 -browser browserName=chrome"
-mate-terminal -e "java -jar server.jar -role webdriver -hub http://localhost:4444/grid/register -port 5552 -browser browserName=chrome"
-mate-terminal -e "java -jar server.jar -role webdriver -hub http://localhost:4444/grid/register -port 5553 -browser browserName=chrome"
-mate-terminal -e "java -jar server.jar -role webdriver -hub http://localhost:4444/grid/register -port 5554 -browser browserName=chrome"
-mate-terminal -e "java -jar server.jar -role webdriver -hub http://localhost:4444/grid/register -port 5555 -browser browserName=chrome"
+xterm -geometry 150x30 -e java -jar server.jar -role webdriver -hub http://localhost:4444/grid/register -port 5551 -browser browserName=chrome &
+xterm -geometry 150x30 -e java -jar server.jar -role webdriver -hub http://localhost:4444/grid/register -port 5552 -browser browserName=chrome &
+xterm -geometry 150x30 -e java -jar server.jar -role webdriver -hub http://localhost:4444/grid/register -port 5553 -browser browserName=chrome &
+xterm -geometry 150x30 -e java -jar server.jar -role webdriver -hub http://localhost:4444/grid/register -port 5554 -browser browserName=chrome &
+xterm -geometry 150x30 -e java -jar server.jar -role webdriver -hub http://localhost:4444/grid/register -port 5555 -browser browserName=chrome &
