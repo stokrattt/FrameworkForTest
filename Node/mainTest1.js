ac6b79f4c2acdda9e9cf291e7f33084d17d19bcd
stokratt <stokratt@gmail.com>
2017-03-12 18:22:44 +0200

    add longFunctions and screenshots after fail

--- Node/mainTest1.js
+++ Node/mainTest1.js
@@ -1,9 +1,9 @@
 //======================check for debug mode=============================
-var attrs=2;
-global.D=false;
-if ('-d' == process.argv[attrs]){
+var attrs = 2;
+global.D = false;
+if ('-d' == process.argv[attrs]) {
     console.log('debug enabled')
-    D=true;
+    D = true;
     attrs++;
 }
 //=====================set up bebDriver==================================
@@ -11,32 +11,40 @@ var webdriver = require('selenium-webdriver');
 global.By = webdriver.By;
 global.until = webdriver.until;
 
-webdriver.promise.controlFlow().on('uncaughtException', function (e) {
-    console.log('Произошла ошибка: ', e);
-});
-
 var SELENIUM_HOST = 'http://localhost:4444/wd/hub';
 global.driver = new webdriver.Builder()
     .usingServer(SELENIUM_HOST)
     .withCapabilities({browserName: 'chrome'})
     .build();
 
+webdriver.promise.controlFlow().on('uncaughtException', function (e) {
+    driver.takeScreenshot().then(
+        function (image, err) {
+            require('fs').writeFile('out.png', image, 'base64', function (err) {
+                console.log(err);
+            });
+            console.log('сделали скрин');
+            console.log('Произошла ошибка: ', e);
+        });
+});
+
 //========================set up global variables========================
 global.Fiber = require('fibers');
 global.busy = false;
 global.Dtimeout = 10000;
-global.V={};
+global.V = {};
 global.Debug = require("./DebugWD.js");
 var SF = require('./ShortFunctionsWD.js');
 var JSs = require('./JSsteps');
+var LF = require('./LongFunctionsWD.js');
 if (D) {
     Debug.console();
     Debug.pauseWatcher();
 }
 //=================reading parametres from CLI===========================
-for (attrs; attrs<process.argv.length; attrs++) {
-    if (process.argv[attrs].indexOf('=')!=-1){
-        global[process.argv[attrs].substring(0, process.argv[attrs].indexOf('='))]=process.argv[attrs].substring(process.argv[attrs].indexOf('=')+1);
+for (attrs; attrs < process.argv.length; attrs++) {
+    if (process.argv[attrs].indexOf('=') != -1) {
+        global[process.argv[attrs].substring(0, process.argv[attrs].indexOf('='))] = process.argv[attrs].substring(process.argv[attrs].indexOf('=') + 1);
     }
 }
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
--- Node/tests/createLocalMoving.js
+++ Node/tests/createLocalMoving.js
@@ -3,30 +3,7 @@ function main(){
     global.fiber = Fiber.current;
     var URL = 'http://stage.themoveboard.com/';
     driver.get(URL);
-    SFsend(By.xpath('//ultrasmall-form//input[@ng-model="request.zipFrom"]'), "02461");
-    SFsend(By.xpath('//ultrasmall-form//input[@ng-model="request.zipTo"]'), "02111");
-    //Debug.pause();
-    driver.wait(driver.executeScript("$('ultrasmall-form input[ng-model=\"moveDate\"]').focus();"));
-    driver.sleep(2000);
-    V.Future = null;
-    driver.wait(driver.executeScript(Click4DaysNewCalendar).then(function (D) {
-        V.Future = D;
-    }));
-    waitForDefined("Future");
-    driver.executeScript("$('ultrasmall-form input[ng-click=\"Continue1(\\\'step1\\\')\"]').click();");
-    JSclick("ultrasmall-form div[ng-click='openSlide();']");
-    JSclick("div[ng-click='MoveSizePreviewClick(\\\\\'4\\\\\')']");
-    JSclick("button.pull-right:first");
-    JSselect('ultrasmall-form select[ng-model="request.typeFrom"]',4);
-    JSselect('ultrasmall-form select[ng-model="request.typeTo"]',3);
-    JSclick('input[ng-click=\\"Continue2(\'step2\')\\"]');
-    SFsend(By.xpath('//ultrasmall-form//input[@ng-model="request.first_name"]'), 'TestName');
-    SFsend(By.xpath('//ultrasmall-form//input[@ng-model="request.last_name"]'), 'TestFam');
-    SFsend(By.xpath('//ultrasmall-form//input[@ng-model="request.email"]'), 'first@email.com');
-    SFsend(By.xpath('//ultrasmall-form//input[@ng-model="request.primaryPhone"]'), '8475960443');
-    JSclick('input[ng-click=\\"Calculate(\\\'Submit\\\')\\"]');
-    JSwait('ultrasmall-form #congrats_menu[style="right: 0px;"] a:contains("Proceed To View Your Quote")');
-    JSlink('ultrasmall-form a:contains("Proceed To View Your Quote")');
+    FullSmallCalcAsLocal();
     console.log('мы типа в конце');
 }
 
