092fb441416c516b78d5d7376fb2676c7956ac8b
Mihail <riba-kit17@mail.ru>
2017-04-10 14:37:11 +0300

    Cleaner

---
+++ Node/tests/TheCleaner.js
@@ -0,0 +1,58 @@
+module.exports = function main(SF, JS, JSstep, VD, V, By, until,FileDetector, system, condition, LF,config,constants){
+    global.fiber = Fiber.current;
+    //=========================начинаем писать тест=============================
+    if (V.cleanerJob.length!=0) {
+        SF.get(V.adminURL);
+        LF.LoginToBoardAsAdmin();
+        for (let i = 0; i < V.cleanerJob.length; i++) {
+            SF.clear(By.xpath('//input[@ng-model="search"]'));
+            SF.send(By.xpath('//input[@ng-model="search"]'), V.cleanerJob[i]);
+            SF.waitForVisible(By.xpath('//div[@ng-bind-html="request.nid | searchfilter:search"]/span[contains(text(),"' + V.cleanerJob[i] + '")]'));
+            SF.click(By.xpath('//div[@ng-bind-html="request.nid | searchfilter:search"]/span[contains(text(),"' + V.cleanerJob[i] + '")]/..'));
+            SF.waitForVisible(By.xpath('//div[@ng-click="chooseTruck(tid)"]'));
+            JS.waitForNotExist('div.busyoverlay:visible');
+            SF.sleep(2);
+
+            let Release = 0;
+            driver.wait(driver.findElements(By.xpath('//span[@ng-click="ReleaseEdit()"]')).then(function (array) {
+                Release = array.length;
+            }), config.timeout);
+            SF.sleep(1);
+            if (Release != 0) {
+                SF.click(By.xpath('//span[@ng-click="ReleaseEdit()"]'));
+                SF.sleep(1);
+                JS.waitForNotExist('div.busyoverlay:visible');
+            }
+            let Closed = 0;
+            driver.wait(driver.findElements(By.xpath('//div[@ng-if="Job_Completed"][@ng-click="openJob();"]')).then(function (array) {
+                Closed = array.length;
+            }), config.timeout);
+            SF.sleep(1);
+            if (Closed != 0) {
+                SF.click(By.xpath('//div[@ng-if="Job_Completed"][@ng-click="openJob();"]'));
+                SF.sleep(1);
+                JS.waitForNotExist('div.busyoverlay:visible');
+            }
+            let Sales = 0;
+            driver.wait(driver.findElements(By.xpath('//div[contains(@class,"sales_type")][contains(@class,"active")]')).then(function (array) {
+                Sales = array.length;
+            }), config.timeout);
+            SF.sleep(1);
+            if (Sales == 0) {
+                SF.click(By.xpath('//div[contains(@class,"sales_type")]'));
+                SF.sleep(1);
+                JS.waitForNotExist('div.busyoverlay:visible');
+            }
+
+            SF.select(By.xpath('//select[@id="edit-status"]'), 14);
+            SF.click(By.xpath('//button[@ng-click="UpdateRequest()"]'));
+            JS.waitForExist('button[ng-click="update(request)"]:visible');
+            SF.click(By.xpath('//button[@ng-click="update(request)"]'));
+            JS.waitForNotExist("div.busyoverlay:visible");
+            LF.closeEditRequest();
+            SF.sleep(2);
+        }
+    }
+    //=========================закончили писать тест=============================
+    SF.endOfTest();
+};
