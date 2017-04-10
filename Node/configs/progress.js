0d6fef1314a1affa13484d04587221c80b6b363f
Mihail <riba-kit17@mail.ru>
2017-04-10 14:37:31 +0300

    a

--- Node/configs/progress.js
+++ Node/configs/progress.js
@@ -1,5 +1,5 @@
 module.exports = function(config,V) {
-    config.suite = ['./tests/SignForemanJobAndUnssignetJob.js'];
+    config.suite = ['./tests/TheCleaner.js'];
     config.chainFail = false;
     V.frontURL = 'http://stage.themoveboard.com:8001/front_site/';
     V.accountURL = 'http://stage.themoveboard.com:8001/account/#/login';
