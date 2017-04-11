08581f6d8eb65e75c0030b34bfffd2c7ae7719ae
Mihail <riba-kit17@mail.ru>
2017-04-11 19:05:16 +0300

    my config

---
+++ Node/configs/RibaKit.js
@@ -0,0 +1,9 @@
+module.exports = function(config,V) {
+    config.suite = ['./tests/allMovingWithStorage.js'];
+    config.chainFail = false;
+    V.frontURL = 'http://stage.themoveboard.com:8001/front_site/';
+    V.accountURL = 'http://stage.themoveboard.com:8001/account/#/login';
+    V.adminURL = 'http://stage.themoveboard.com:8001/moveBoard/#/login';
+};
+
+
