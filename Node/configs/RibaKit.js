c92a3761389cbde543ae03bc4115b5a70c1b45cc
Mihail <riba-kit17@mail.ru>
2017-04-26 20:20:46 +0300

    finish 38

--- Node/configs/RibaKit.js
+++ Node/configs/RibaKit.js
@@ -1,6 +1,6 @@
 module.exports = function(config,V) {
     config.suite = [
-        './tests/PendingStorage.js'
+        './tests/38_CustomPayrollInRequest.js'
     ];
     config.chainFail = false;
     V.frontURL = 'http://stage.themoveboard.com:8001/front_site/';
