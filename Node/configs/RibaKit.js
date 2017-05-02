688bef9f8c79ff9c90c714063452c66bfe555bf9
Mihail <riba-kit17@mail.ru>
2017-05-02 18:00:40 +0300

    remake 5 done

--- Node/configs/RibaKit.js
+++ Node/configs/RibaKit.js
@@ -1,11 +1,14 @@
 module.exports = function(config,V) {
     config.suite = [
-        './tests/1_AllLocalMoving.js'
+        './tests/5_ReservationMov&Stor.js'
     ];
     config.chainFail = false;
-    V.frontURL = 'http://stage.themoveboard.com:8001/front_site/';
-    V.accountURL = 'http://stage.themoveboard.com:8001/account/#/login';
-    V.adminURL = 'http://stage.themoveboard.com:8001/moveBoard/#/login';
+    V.adminURL = 'localhost:8080/moveBoard/#/dashboard';
+    V.accountURL = 'localhost:8080/account/#/login';
+    V.frontURL = 'localhost:8080/front_site/esquiremoving/';
+    //V.frontURL = 'http://stage.themoveboard.com:8001/front_site/';
+    //V.accountURL = 'http://stage.themoveboard.com:8001/account/#/login';
+    //V.adminURL = 'http://stage.themoveboard.com:8001/moveBoard/#/login';
 };
 
 
--- Node/tests/5_ReservationMov&Stor.js
+++ Node/tests/5_ReservationMov&Stor.js
@@ -32,45 +32,14 @@ module.exports = function main(SF, JS, MF, LF, JSstep, VD, V, By, until,FileDete
     condition.nowWeDoing = 'зашли под клиентом в акк';
     SF.get(V.accountURL);
     LF.LoginToAccountAsClient (V.client, V.client.passwd);
-    MF.Account_CheckRequestStatus_NotConfirmed(V.accountNumbers.Id);
-    MF.Account_OpenRequest(V.accountNumbers.Id);
-    SF.waitForVisible (By.xpath('//button[@ng-click="cancel()"]'));
-    SF.click (By.xpath('//button[@ng-click="cancel()"]'));
-    SF.sleep (0.5);
-    SF.click (By.xpath('//div[@class="field-status notconfirmed ng-scope"]/a'));
-    SF.click (By.xpath('//i[@class="fa fa-angle-down arrow-down"]'));
-    SF.sleep (0.5);
-    SF.click (By.id('terms'));
-    SF.click (By.id('cancel_policy'));
-    driver.wait(driver.findElement(By.xpath('//div[@class="confirm ng-scope"]')).getText().then(function(deposit){
-        V.DepositReservation = SF.cleanPrice (deposit);
-    }), config.timeout);
-    SF.sleep (2);
-    VD.IWant (VD.VToEqual, V.DepositReservation, V.ReservationPrice, 'Резервация которую мы выставили в настройках не совпадает');
-    SF.click (By.id('paybutton'));
-    SF.waitForVisible (By.xpath('//div[@class="sweet-alert showSweetAlert visible"]'));
-    SF.click (By.xpath('//button[@class="confirm"]'));
-    SF.waitForVisible (By.xpath('//div[@class="modal-body form-horizontal"]'));
-    SF.send (By.id('edit-moving-from'), 'otkuda edem');
-    SF.send (By.id('edit-moving-from-apt'), 324535);
-    SF.click (By.xpath('//button[@ng-click="update(client)"]'));
-
-    SF.waitForVisible (By.xpath('//div[@class="sweet-alert showSweetAlert visible"]'));
-    SF.click (By.xpath('//button[@class="confirm"]'));
-
-    SF.waitForVisible(By.xpath('//canvas[@id="signatureCanvasReserv"]'));
-    LF.MakeSignJS('signatureCanvasReserv');
-    SF.sleep(3);
-    JS.waitForExist ('div[class=\\"sweet-overlay\\"]:visible');
-    SF.click (By.xpath('//button[@class="confirm"]'));
-    SF.click(By.xpath('//button[@ng-click="saveReservSignature();logClickButtons(\'Save reservation sign button clicked\')"]'));
-    SF.sleep (1);
-    LF.FillCardPayModal ();
-    SF.waitForVisible (By.xpath('//div[@class="field-status confirm ng-scope"]'));
-    driver.wait(driver.findElement(By.xpath('//div[@class="field-status confirm ng-scope"]/div')).getText().then(function(confirmed){
-        VD.IWant (VD.VToEqual, confirmed, 'YOUR MOVE IS CONFIRMED AND SCHEDULED', 'статус не конферм, хотя должен был быть');
-    }), config.timeout);
+    MF.Account_CheckRequestStatus_NotConfirmed(V.boardNumbers.Id);
+    MF.Account_OpenRequest(V.boardNumbers.Id);
+    Debug.pause();
+    MF.Account_ClickViewRequest();
+    MF.WaitWhileBusy();
+    SF.sleep(5);
+    MF.WaitWhileBusy();
+    LF.ConfirmRequestInAccount_WithReservation();
     LF.LogoutFromAccount ();
-
     SF.endOfTest();
 };
