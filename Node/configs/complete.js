98abecdba2edb22dbe8ea71a16c326fb81a8af1b
stokratt <stokratt@gmail.com>
2017-05-08 15:42:30 +0200

    add new 44 test assign create foreman

--- Node/configs/complete.js
+++ Node/configs/complete.js
@@ -54,7 +54,8 @@ module.exports = function(config,V) {
         './tests/40_PayrollTableCheckSumJobs.js',
         './tests/41_ContractPageSettings.js',
         './tests/42_DrivingTimeLocalMove.js',
-        './tests/43_WeightType.js'
+        './tests/43_WeightType.js',
+        './tests/44_CreateForemanAndAssignCheck.js'
 
 
     ];
--- Node/configs/progress.js
+++ Node/configs/progress.js
@@ -1,5 +1,5 @@
 module.exports = function(config,V) {
-    config.suite = ['./tests/43_WeightType.js'];
+    config.suite = ['./tests/44_CreateForemanAndAssignCheck.js'];
     config.chainFail = false;
     config.browser = 'chrome';
 
