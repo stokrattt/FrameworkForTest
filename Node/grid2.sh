5d8df8abca4ea5463a904094126f3d35ba18ded0
Mikhail <ribakit@136345.simplecloud.club>
2017-05-17 07:29:57 +0000

    grid 2

---
+++ Node/grid2.sh
@@ -0,0 +1,6 @@
+xterm -geometry 100x25 -bg black -e "java -jar server.jar -role hub"&
+sleep 10
+xterm -geometry 100x25 -bg blue -e "java -jar server.jar -role webdriver -hub http://localhost:4444/grid/register -port 5561 -browser browserName=chrome"&
+xterm -geometry 100x25 -bg blue -e "java -jar server.jar -role webdriver -hub http://localhost:4444/grid/register -port 5562 -browser browserName=chrome"&
+sleep 10
+bash ./startTests.sh
