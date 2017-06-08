21d7d78adfa0269599f3075cb564dbb4ee69773c
stokratt <stokratt@gmail.com>
2017-06-08 12:18:19 +0200

    58 OK

--- Node/configs/progress.js
+++ Node/configs/progress.js
@@ -1,5 +1,5 @@
 module.exports = function(config,V) {
-    config.suite = ['./tests/48_Review.js'];
+    config.suite = ['./tests/58_Discount.js'];
     config.chainFail = false;
     config.browser = 'chrome';
     config.timeout = 30000;
