91b100ca2d1d797d4c619d83cc1b54c58a816f9d
Mihail <riba-kit17@mail.ru>
2017-06-15 16:37:15 +0300

    commented 1 signature in contract in test#1

--- Node/common/LongFunctionsWD.js
+++ Node/common/LongFunctionsWD.js
@@ -1420,7 +1420,6 @@ module.exports = function (SF, JS, MF, JSstep, VD, V, By, until,FileDetector, sy
         VD.IWant(VD.VToEqual, Math.floor(boardNumbers.Payroll.foremanForCommission.Bonus.percent),
             Math.floor(10),
             'Не совпал Bonus формена');
-        Debug.pause();
         VD.IWant(VD.VToEqual, Math.floor(boardNumbers.Payroll.foremanForCommission.Bonus.forCommission),
             Math.floor(boardNumbers.Quote/boardNumbers.HourlyRate),
             'Не совпал LaborTime формена');
