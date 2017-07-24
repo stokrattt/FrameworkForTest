3afae184ea2cc4f4ba4aedb4b56c86311660bf94
Dima <dmitryrybin89@gmail.com>
2017-07-24 22:12:08 +0300

    Using Crew Settings  must be 4 signatureы for DETAILS OF LABOR CHARGES on Contract

---
+++ Node/tests/76_ContractWith4Signature.js
@@ -0,0 +1,138 @@
+module.exports = function main(SF, JS, MF, LF, JSstep, VD, V, By, until,FileDetector, system, condition, config,constants){
+    global.fiber = Fiber.current;
+    V.client = {};
+    V.client.name = SF.randomBukva(6) + '_t';
+    V.client.fam = SF.randomBukva(6) + '_t';
+    V.client.phone = SF.randomCifra(10);
+    V.client.email = SF.randomBukvaSmall(6) + '@' + SF.randomBukvaSmall(4) + '.tes';
+    V.client.passwd = 123;
+    SF.get(V.frontURL);
+    condition.nowWeDoing = 'заполняем калькулятор верхний';
+    LF.FullSmallCalcAsLocal(V.client);
+
+    condition.nowWeDoing = 'первый раз в аккаунте';
+    MF.Account_ClickViewRequest();
+    MF.WaitWhileBusy();
+    SF.sleep(5);
+    MF.WaitWhileBusy();
+    V.accountNumbers={};
+    LF.RememberAccountNumbers(V.accountNumbers);
+    LF.addToCleanerJob(V.accountNumbers.Id);
+    LF.LogoutFromAccount();
+
+    condition.nowWeDoing = 'первый раз в админке';
+    SF.get(V.adminURL);
+    LF.LoginToBoardAsCustom(V.adminLogin,V.adminPassword);
+    MF.Board_OpenRequest(V.accountNumbers.Id);
+    V.boardNumbers = {};
+    LF.RememberDigitsRequestBoard(V.boardNumbers);
+    LF.Validation_Compare_Account_Admin(V.accountNumbers, V.boardNumbers);
+    JS.step(JSstep.selectTruck((V.boardNumbers.LaborTimeMax + V.boardNumbers.TravelTime)/60));
+    V.managerFirstName = 'dima';
+
+    MF.EditRequest_OpenSettings();
+    LF.SetManager(V.managerFirstName);
+    MF.EditRequest_OpenClient();
+    LF.SetClientPasswd(V.client.passwd);
+    MF.EditRequest_OpenRequest();
+    MF.EditRequest_SetToNotConfirmed();
+    MF.EditRequest_SaveChanges();
+    LF.closeEditRequest();
+    SF.sleep(2);
+    MF.Board_LogoutAdmin();
+
+    condition.nowWeDoing = 'второй раз в аккаунте, конфёрмим';
+    SF.get(V.accountURL);
+    LF.LoginToAccountAsClient(V.client);
+    MF.Account_CheckRequestStatus_NotConfirmed (V.accountNumbers.Id);
+    MF.Account_OpenRequest (V.accountNumbers.Id);
+    SF.click (By.xpath('//div[@class="field-status notconfirmed ng-scope"]/a'));
+    SF.sleep(2);
+    SF.click (By.xpath('//i[@class="fa fa-angle-down arrow-down"]'));
+    SF.sleep (0.5);
+    SF.click (By.id('terms'));
+    SF.click (By.id('cancel_policy'));
+    SF.click (By.id('paybutton'));
+    MF.SweetConfirm();
+    SF.waitForVisible (By.xpath('//div[@class="modal-body form-horizontal"]'));
+    SF.sleep(2);
+    SF.send (By.id('edit-moving-from'), 'otkuda edem');
+    SF.send (By.id('edit-moving-from-apt'), 324535);
+    SF.send (By.xpath('//input[@ng-model="request.field_moving_to.thoroughfare"]'), 'kuda edem');
+    SF.send (By.xpath('//input[@ng-value="request.apt_to.value"]'), 324535);
+    SF.click (By.xpath('//button[@ng-click="update(client)"]'));
+    SF.sleep(2);
+    MF.SweetConfirm();
+    MF.SweetConfirm();
+    SF.waitForVisible(By.xpath('//canvas[@id="signatureCanvasReserv"]'));
+    LF.MakeSignJS('signatureCanvasReserv');
+    SF.sleep(0.5);
+    SF.click(By.xpath('//button[@ng-click="saveReservSignature();logClickButtons(\'Save reservation sign button clicked\')"]'));
+    SF.sleep (1);
+    LF.FillCardPayModal ();
+    MF.WaitWhileSpinner ();
+    SF.waitForVisible (By.xpath('//div[@class="field-status confirm ng-scope"]'));
+    driver.wait(driver.findElement(By.xpath('//div[@class="field-status confirm ng-scope"]/div')).getText().then(function(confirmed){
+        VD.IWant (VD.ToEqual, confirmed, 'YOUR MOVE IS CONFIRMED AND SCHEDULED', 'статус не конферм, хотя должен был быть');
+    }), config.timeout);
+    LF.LogoutFromAccount ();
+
+    condition.nowWeDoing = 'второй раз в админке, локал диспатч';
+    SF.get(V.adminURL);
+    LF.LoginToBoardAsCustom(V.adminLogin,V.adminPassword);
+    MF.Board_OpenLocalDispatch();
+    LF.findDayInLocalDispatch(V.boardNumbers.moveDate.Year, V.boardNumbers.moveDate.Month, V.boardNumbers.moveDate.Day);
+    MF.WaitWhileBusy();
+    MF.WaitWhileBusy();
+    MF.Dispatch_GridView();
+    LF.SelectRequestDispatch(V.accountNumbers.Id);
+    LF.selectCrew(V.foremanName);
+    SF.click(By.xpath("//a[@ng-click='vm.openSettingsModal()']"));
+    SF.sleep (5);
+    SF.click(By.xpath("//input[@name='pickup']"));
+    SF.sleep (1);
+    SF.click(By.xpath("//button[@ng-click='saveSettings()']"));
+    SF.sleep (5);
+    MF.Board_LogoutAdmin();
+
+    condition.nowWeDoing = 'заходим под форменом, открываем контракт';
+    LF.LoginToBoardAsCustomForeman(V.foremanLogin, V.foremanPassword);
+    LF.OpenRequestDispatch(V.accountNumbers.Id);
+    MF.Contract_WaitConfirmationPage();
+    MF.Contract_OpenBillOfLading();
+    SF.sleep(1);
+    LF.MakeSignInContract();
+    LF.MakeSignInContract();
+    MF.Contract_DeclarationValueA();
+    LF.MakeSignInContract();
+    LF.MakeSignInContract();
+    LF.MakeSignInContract();
+    LF.MakeSignInContract();
+    LF.MakeSignInContract();
+    MF.Contract_ClickPay();
+    MF.Contract_ClickTips10();
+    MF.Contract_ClickAddTips();
+    MF.Contract_ClickPaymentInfo();
+    LF.FillCardPayModal();
+    LF.Contract_SignMainPayment();
+    driver.wait(new FileDetector().handleFile(driver, system.path.resolve('./files/squirrel.jpg')).then(function (path) {
+        V.path = path;
+    }), config.timeout);
+    SF.sleep(1);
+    MF.Contract_UploadImage(V.path);
+    MF.Contract_UploadImage(V.path);
+    MF.Contract_SaveImages();
+    LF.MakeSignInContract();
+    LF.MakeSignInContract();
+    MF.Contract_Submit();
+    MF.Contract_ReturnToForeman();
+    LF.LogoutFromBoardForeman();
+
+
+
+    SF.endOfTest();
+};
+
+
+
+
