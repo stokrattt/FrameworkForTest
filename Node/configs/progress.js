779f59a6f56a175957a76b98ec23d368e41f768b
stokratt <stokratt@gmail.com>
2017-09-15 13:03:14 +0300

    delete some test

--- Node/configs/progress.js
+++ Node/configs/progress.js
@@ -1,6 +1,6 @@
 module.exports = function(config,V) {
 
-    config.suite = ['./tests/58_Discount.js'];
+    config.suite = ['./tests/59_PayrollAddMiscCustomPayment.js'];
     config.chainFail = false;
     config.browser = 'chrome';
     config.timeout = 35000;
