d7de4c0eb036354e57b870d643b09769073683f9
stokratt <stokratt@gmail.com>
2017-10-11 12:10:10 +0300

    fix schedule

--- Node/tests/49_FlatRateLocalMoveCheckSchedule.js
+++ Node/tests/49_FlatRateLocalMoveCheckSchedule.js
@@ -222,7 +222,7 @@ condition.nowWeDoing = 'выбираем цифры helper';
         MF.WaitWhileBusy();
         SF.sleep(5);
         driver.wait(driver.executeScript("return $('div.line1:contains("+V.request.Id+")').length").then (function (checkSchedule) {
-            VD.INeed(VD.ToEqual, checkSchedule, 4, 'работа не нашлась в schedule по move date, реквест номер '+V.request.Id+'');
+            VD.INeed(VD.ToEqual, checkSchedule, 2, 'работа не нашлась в schedule по move date, реквест номер '+V.request.Id+'');
         }),config.timeout);
     } else {
         SF.click(By.xpath('//i[@ng-click="vm.nextMonth()"]'));
@@ -230,7 +230,7 @@ condition.nowWeDoing = 'выбираем цифры helper';
         SF.click(By.xpath('//div[contains(@class, "cal-day-inmonth")]/span[contains(@class, "pull-right")] and [contains(text(), "' + V.datescedule + '")]'));
         SF.sleep(5);
         driver.wait(driver.executeScript("return $('div.line1:contains("+V.request.Id+")').length").then (function (checkSchedule) {
-            VD.INeed(VD.ToEqual, checkSchedule, 4, '(проверка два) работа не нашлась в schedule по move date, реквест номер '+V.request.Id+'');
+            VD.INeed(VD.ToEqual, checkSchedule, 2, '(проверка два) работа не нашлась в schedule по move date, реквест номер '+V.request.Id+'');
         }),config.timeout);
     }
     SF.sleep(2);
