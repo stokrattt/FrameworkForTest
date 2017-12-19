717a896072a7982cddbc77a17692ab3f33a28722
stokratt <stokratt@gmail.com>
2017-12-19 19:24:28 +0200

    fix payrol xpath for flat rate

--- Node/common/LongFunctionsWD.js
+++ Node/common/LongFunctionsWD.js
@@ -1538,7 +1538,7 @@ module.exports = function (SF, JS, MF, JSstep, VD, V, By, until,FileDetector, sy
         });
         SF.sleep(1);
         MF.EditRequest_PayrollOpenHelperTab();
-        driver.wait(driver.findElement(By.xpath('//label[@ng-init="calcWorkerTotal(\'helper\'); calcWorkerTotal(\'foremanAsHelper\')"]')).getText().then(function (text) {
+        driver.wait(driver.findElement(By.xpath('//label[@ng-init="calcWorkerTotal(\'helper\'); calcWorkerTotal(\'foremanAsHelper\'); calcWorkerTotal(\'driver\');"]')).getText().then(function (text) {
             boardNumbers.Payroll.helpersForComission.total = SF.cleanPrice(text);
         }),config.timeout);
         SF.sleep(1);
@@ -1586,7 +1586,7 @@ module.exports = function (SF, JS, MF, JSstep, VD, V, By, until,FileDetector, sy
         });
         SF.sleep(1);
         MF.EditRequest_PayrollOpenHelperTab();
-        driver.wait(driver.findElement(By.xpath('//label[@ng-init="calcWorkerTotal(\'helper\'); calcWorkerTotal(\'foremanAsHelper\')"]')).getText().then(function (text) {
+        driver.wait(driver.findElement(By.xpath('//label[@ng-init="calcWorkerTotal(\'helper\'); calcWorkerTotal(\'foremanAsHelper\'); calcWorkerTotal(\'driver\');"]')).getText().then(function (text) {
             boardNumbers.Payroll.helpersForComission.total = SF.cleanPrice(text);
         }),config.timeout);
         SF.sleep(1);
