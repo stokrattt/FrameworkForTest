607bc11de684474e4985039f8693f40e2019ccea
Dima <dmitryrybin89@gmail.com>
2018-01-29 14:49:46 +0200

    change and test

--- Node/tests/112_ExtraDropLocalMove.js
+++ Node/tests/112_ExtraDropLocalMove.js
@@ -5,8 +5,7 @@ module.exports = function main(SF, JS, MF, LF, JSstep, VD, V, By, until,FileDete
     V.client.fam = SF.randomBukva(6) + '_t';
     V.client.phone = SF.randomCifra(10);
     V.client.email = SF.randomBukvaSmall(6) + '@' + SF.randomBukvaSmall(4) + '.tes';
-    V.extraPickUpAddress = SF.randomBukva(4);
-    V.extraDropOffAddress = SF.randomBukva(4);
+
 
 
     SF.get(V.adminURL);
@@ -16,21 +15,27 @@ module.exports = function main(SF, JS, MF, LF, JSstep, VD, V, By, until,FileDete
     LF.CreateLocalMovingFromBoard(V.client);
     SF.click(By.xpath('//i[@ng-click="request.extraPickup=true"]'));
     SF.sleep(1);
+    V.extraPickUpAddress = SF.randomBukva(6) + '_t';
+    V.extraDropOffAddress = SF.randomBukva(6) + '_t';
+    V.extraPickUpApt = 123;
+    V.extraDropOffpApt = 321;
     SF.send(By.xpath('//input[@ng-model="request.field_extra_pickup.thoroughfare"]'),V.extraPickUpAddress);
-    SF.click(By.xpath('//input[@id="edit-moving-from-apt"]'));
-    SF.send(By.xpath('//input[@id="edit-moving-from-apt"]'),12);
+    SF.send(By.xpath('//input[@ng-model="request.field_extra_pickup.postal_code"]'),'01101');
+    SF.send(By.xpath('//input[@ng-model="request.field_extra_pickup.premise"]'), V.extraPickUpApt);
     SF.select(By.xpath('//select[@ng-change="changeRequestField(\'field_extra_pickup\')"]'),3);
+    SF.sleep(2);
     SF.click(By.xpath('//i[@ng-click="request.extraDropoff=true"]'));
     SF.sleep(1);
     SF.send(By.xpath('//input[@ng-model="request.field_extra_dropoff.thoroughfare"]'),V.extraDropOffAddress);
-    SF.send(By.xpath('//input[@ng-model="request.field_extra_dropoff.premise"]'), 21);
+    SF.send(By.xpath('//input[@ng-model="request.field_extra_dropoff.postal_code"]'),'02222');
+    SF.send(By.xpath('//input[@ng-model="request.field_extra_dropoff.premise"]'), V.extraDropOffpApt);
     SF.select(By.xpath('//select[@ng-change="changeRequestField(\'field_extra_dropoff\')"]'),3);
     MF.EditRequest_SaveChanges();
     V.boardNumbers = {};
     LF.RememberDigitsRequestBoard(V.boardNumbers);
     LF.closeEditRequest();
 
-    condition.nowWeDoing = 'заходим заново в реквест, и проверяем сохранились ли сервисы';
+    condition.nowWeDoing = 'заходим заново в реквест, и проверяем сохранились ли сервисы, меняем этажи';
     MF.Board_RefreshDashboard();
     MF.Board_OpenRequest (V.boardNumbers.Id);
     driver.wait(driver.findElement(By.xpath('//div//h4[contains(text(),"Extra Drop-off")]')).getText().then(function(text) {
@@ -39,24 +44,58 @@ module.exports = function main(SF, JS, MF, LF, JSstep, VD, V, By, until,FileDete
     }),config.timeout);
     SF.sleep(1);
     driver.wait(driver.findElement(By.xpath('//div//h4[contains(text(),"Extra Pickup")]')).getText().then(function(text) {
-        V.ExtraPickUp = SF.cleanPrice(text);
-        VD.IWant(VD.ToEqual, V.ExtraPickUp, text,'нет сервиса экстра прикап');
+        V.ExtraPickU = text;
+        VD.IWant(VD.ToEqual, V.ExtraPickU, text,'нет сервиса экстра пикап');
     }),config.timeout);
     SF.sleep(1);
+    SF.select(By.xpath('//select[@ng-change="changeRequestField(\'field_extra_pickup\')"]'),2);
+    SF.select(By.xpath('//select[@ng-change="changeRequestField(\'field_extra_dropoff\')"]'),2);
+    MF.EditRequest_SaveChanges();
+    V.boardNumbers = {};
+    LF.RememberDigitsRequestBoard(V.boardNumbers);
 
 
-    condition.nowWeDoing = 'идем в аккаунт, и на конфирмеишен от админа проверяем экстра';
-    //MF.EditRequest_OpenSettings();
-    //MF.EditRequest_ClickViewRequest();
-    //SF.sleep(10);
-   // SF.openTab(1);
-  //  SF.sleep(2);
-  //  driver.close();
-   // SF.openTab(0);
-   // SF.waitForLocated(By.xpath('//button[@ng-click="goToConfirmation()"]'));
-   // SF.click (By.xpath('//button[@ng-click="goToConfirmation()"]'));
-   // SF.openTab(1);
-   // SF.sleep(2);
+    condition.nowWeDoing = 'идем в аккаунт, и на конфирмеишен от админа проверяем эналичие экстра сервисов ';
+    MF.EditRequest_OpenSettings();
+    MF.EditRequest_ClickViewRequest();
+    SF.sleep(12);
+    SF.openTab(1);
+    driver.wait(driver.findElement(By.xpath('//div[contains(text(), "Extra Drop-off")]')).getText().then(function(text){
+        V.extraDropOffAcc = text;
+        VD.IWant(VD.ToEqual, V.extraDropOffAcc , V.extraDropOffAcc, 'нет ектра дроп офф на аккаунте');
+    }),config.timeout);
+    SF.sleep(2);
+    driver.wait(driver.findElement(By.xpath('//div[contains(text(), "Extra Pickup:")]')).getText().then(function(text){
+        V.extraPickUpAcc = text;
+        VD.IWant(VD.ToEqual, (V.ExtraPickU  + ':'), text, 'нет екстра пик ап на аккаунте');
+    }),config.timeout);
+    SF.sleep(2);
+    driver.wait(driver.findElement(By.xpath('//div[@ng-if="extraPickup"]//div[contains(text(), "Stairs - 2nd Floor")]')).getText().then(function(text){
+        V.extraPickNewStairs = text;
+        VD.IWant(VD.ToEqual, V.extraPickNewStairs, 'Stairs - 2nd Floor', 'не совпали этажи с админкой и аккаунтом пикап ');
+    }),config.timeout);
+    SF.sleep(2);
+    driver.wait(driver.findElement(By.xpath('//div[@ng-if="extraPickup"]//div[contains(text(), "Stairs - 2nd Floor")]')).getText().then(function(text){
+        V.extraDropOffNewStairs = text;
+        VD.IWant(VD.ToEqual, V.extraDropOffNewStairs, 'Stairs - 2nd Floor', 'не совпали этажи с админкой и аккаунтом дропоф');
+    }),config.timeout);
+    SF.sleep(5);
+    driver.close();
+    SF.openTab(0);
+    SF.waitForLocated(By.xpath('//button[@ng-click="goToConfirmation()"]'));
+    SF.click (By.xpath('//button[@ng-click="goToConfirmation()"]'));
+    SF.openTab(1);
+    SF.sleep(12);
+    driver.wait(driver.findElement(By.xpath('//div[contains(text(), "Extra Drop-off")]')).getText().then(function(text){
+        V.extraDropOffConf = text;
+        VD.IWant(VD.ToEqual, V.extraDropOffAcc , text, 'нет ектра дроп офф на конфирмеишен');
+    }),config.timeout);
+    SF.sleep(1);
+    driver.wait(driver.findElement(By.xpath('//div[contains(text(), "Extra Pickup:")]')).getText().then(function(text){
+        V.extraPickUpConf = text;
+        VD.IWant(VD.ToEqual, (V.ExtraPickU  + ':'), text, 'нет екстра дроп оф на конфирмеишен');
+    }),config.timeout);
+    SF.sleep(2);
 
 
 
