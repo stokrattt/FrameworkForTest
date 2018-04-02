4c1366972df634afbaab60b93f9d464c98496539
stokratt <stokratt@gmail.com>
2018-04-02 13:16:27 +0300

    rename test

--- Node/configs/complete.js
+++ Node/configs/complete.js
@@ -125,7 +125,7 @@ module.exports = function(config,V) {
         './tests/117_HomeEstimatePortal.js',
         './tests/111_ZIPCode.js',
         './tests/24_FiltrationMoveDateCreateDate.js',
-        './tests/122_MovingAndLongDistCalcOffCheck.js',
+        './tests/122_MovingAndMovStorageCalcOffCheck.js',
         './tests/51_ServiceTypesOnOff.js',
 
         './tests/56_LoadingAllSteps.js',
--- Node/configs/progress.js
+++ Node/configs/progress.js
@@ -1,6 +1,6 @@
 module.exports = function(config,V) {
 
-    config.suite = ['./tests/122_MovingAndLongDistCalcOffCheck.js'];
+    config.suite = ['./tests/122_MovingAndMovStorageCalcOffCheck.js'];
     config.chainFail = false;
     config.browser = 'chrome';
     config.timeout = 55000;
