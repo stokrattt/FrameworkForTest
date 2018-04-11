555f1f8e1723a1f6fd4345f1ebc8ea527415ea5c
stokratt <stokratt@gmail.com>
2018-04-11 16:28:36 +0300

    fix xpath

--- Node/configs/progress.js
+++ Node/configs/progress.js
@@ -1,6 +1,6 @@
 module.exports = function(config,V) {
 
-    config.suite = ['./tests/26_CloneRequest.js'];
+    config.suite = ['./tests/115_LDCommercialMoveSizeInSIT.js'];
     config.chainFail = false;
     config.browser = 'chrome';
     config.timeout = 25000;
