e2b2713e7dca96de754a1ba763e6be0f51f9056d
HannaSzczyrec <sisyrinchium02@gmail.com>
2018-04-18 09:50:36 +0300

    correction cod in test HomeEstimatePortal

--- Node/configs/Ann.js
+++ Node/configs/Ann.js
@@ -3,7 +3,7 @@ module.exports = function (config, V) {
 	config.timeout = 120000;
 
 	config.suite = [
-		'./tests/126_CheckAdditionalServicesinLD.js'
+		'./tests/117_HomeEstimatePortal.js'
 		// './tests/31_FlateRateBooking.js'
 	];
 	config.chainFail = false;
