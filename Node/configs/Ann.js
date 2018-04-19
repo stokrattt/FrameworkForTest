63b441f779499b04f86870f894032f0eb1db1f44
HannaSzczyrec <sisyrinchium02@gmail.com>
2018-04-19 19:18:30 +0300

    addition actions in 120 test

--- Node/configs/Ann.js
+++ Node/configs/Ann.js
@@ -3,7 +3,7 @@ module.exports = function (config, V) {
 	config.timeout = 120000;
 
 	config.suite = [
-		'./tests/117_HomeEstimatePortal.js'
+		'./tests/120_LDFlag.js'
 		// './tests/31_FlateRateBooking.js'
 	];
 	config.chainFail = false;
