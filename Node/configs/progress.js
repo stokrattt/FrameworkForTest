442f8380a660a84b2ae2bb7dd42971bdfdee8c0a
stokratt <stokratt@gmail.com>
2018-05-21 17:17:42 +0300

    add check to undefined sit closing

--- Node/configs/progress.js
+++ Node/configs/progress.js
@@ -1,6 +1,6 @@
 module.exports = function(config,V) {
 
-    config.suite = ['./tests/27_Receipt.js'];
+    config.suite = ['./tests/109_SITBalance.js'];
     config.chainFail = false;
     config.browser = 'chrome';
     config.timeout = 25000;
