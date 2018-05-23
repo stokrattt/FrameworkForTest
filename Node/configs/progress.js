587bbce3454699aa13e18f1a679fb7ec74d35e49
stokratt <stokratt@gmail.com>
2018-05-23 16:29:54 +0300

    commit sweetconfirm

--- Node/configs/progress.js
+++ Node/configs/progress.js
@@ -1,6 +1,6 @@
 module.exports = function(config,V) {
 
-    config.suite = ['./tests/104_AddBedroomAddRoom.js'];
+    config.suite = ['./tests/12_CreateMov&StorFromFrontDownAndCompare.js'];
     config.chainFail = false;
     config.browser = 'chrome';
     config.timeout = 25000;
