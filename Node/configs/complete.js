0e53e7f94c7e6a9e23fd2a68d70c43eb59f6b89c
stokratt <stokratt@gmail.com>
2018-06-05 18:43:47 +0300

    add big wait because soft тупит

--- Node/configs/complete.js
+++ Node/configs/complete.js
@@ -140,7 +140,7 @@ module.exports = function(config,V) {
         // './tests/107_SallaryProfitLossAndPayroll.js',
 
     ];
-    config.timeout = 50000;
+    config.timeout = 90000;
     config.chainFail = false;
 	config.browser = 'chrome';
     // config.chromeOptions = { "args" : ["--no-sandbox", "--headless", "--disable-gpu"] };
