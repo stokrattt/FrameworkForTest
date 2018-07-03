3bf8b9b728f4e80efda0905865e29516d37f1615
stokratt <stokratt@gmail.com>
2018-07-03 14:06:13 +0300

    fix xpath and logic 130 test

--- Node/configs/progress.js
+++ Node/configs/progress.js
@@ -1,6 +1,6 @@
 module.exports = function(config,V) {
 
-    config.suite = ['./tests/115_LDCommercialMoveSizeInSIT.js'];
+    config.suite = ['./tests/130_LDCheckScheduleDeliveryDateAndContract.js'];
     config.chainFail = false ;
     config.browser = 'chrome';
     config.timeout = 45000;
