8833c054071cefcc035c525a9daca07c08d42182
stokratt <stokratt@gmail.com>
2018-07-16 11:10:41 +0300

    comment payroll test

--- Node/configs/progress.js
+++ Node/configs/progress.js
@@ -1,6 +1,6 @@
 module.exports = function(config,V) {
 
-    config.suite = ['./tests/117_HomeEstimatePortal.js'];
+    config.suite = ['./tests/35_InventoryLocalMovingAndValuation.js'];
     config.chainFail = false ;
     config.browser = 'chrome';
     config.timeout = 45000;
