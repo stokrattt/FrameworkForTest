1384f022d22fd50de360b84c433ec98532157c18
stokratt <stokratt@gmail.com>
2018-07-23 08:43:10 +0300

    uncommit login

--- Node/configs/progress.js
+++ Node/configs/progress.js
@@ -1,6 +1,6 @@
 module.exports = function(config,V) {
 
-    config.suite = ['./tests/37_PendingStorage.js'];
+    config.suite = ['./tests/93_PackingDayFromLocMove.js'];
     config.chainFail = false ;
     config.browser = 'chrome';
     config.timeout = 45000;
