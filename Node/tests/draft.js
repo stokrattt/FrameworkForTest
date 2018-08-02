b31d33cf1bb9121dba1cca639dda82e634b88b4e
Hanna <sisyrinchium02@gmail.com>
2018-08-02 22:18:40 +0300

    more sf.sleep in test for e-mail.

--- Node/tests/draft.js
+++ Node/tests/draft.js
@@ -11,76 +11,124 @@ module.exports = function main(SF, JS, MF, LF, JSstep, VD, V, By, until,FileDete
     V.manager.passwd = '123';
 
 
-
-    condition.nowWeDoing = 'создаем флет-рейт, c фронтовой нижней формы';
-    SF.get(V.frontURL);
-    LF.CreateFlatRateDownForm(V.client);
-    MF.Account_ClickViewRequest();
-    LF.AccountFlatRate_ChoosePickupAndDeliveryDate();
-    SF.click(By.xpath('//div[contains(@class, "ng-pristine")]'));
-    LF.AccountFR_SeelectOptions();
-
-    condition.nowWeDoing = 'добавляем инвенторий в акке';
-    LF.AccountFlatRateAddInventory();
-    SF.sleep(1);
-    MF.WaitWhileBusy();
-    driver.wait(driver.findElement(By.xpath('//span[@ng-if="vm.request.field_useweighttype.value == \'2\' && vm.request.inventory_weight.cfs"]')).getText().then(function(text) {
-        V.CBFinAccount = SF.cleanPrice(text);
-    }),config.timeout);
-    MF.Account_SubmitFlatRateAfterAddInventory ();
-    JS.scroll ('a[ng-click=\\"vm.Logout()\\"]');
-    driver.wait(driver.findElement(By.xpath('//div[contains(text(),"Request ID")]/span')).getText().then(function (text) {
-        V.FRId = SF.cleanPrice(text);
-    }),config.timeout);
-    SF.sleep(10);
-    LF.LogoutFromAccount ();
-    SF.get(V.adminURL);
-
-    condition.nowWeDoing = 'идем на админку,заполняем опции,запоминаем резервацию, ставим вручную рейт, выбираем трак,ставим нот конферм';
+    condition.nowWeDoing = 'заходим под менеджером, создаем через криет реквест флет-рейт работу';
     SF.get(V.adminURL);
     LF.LoginToBoardAsCustom(V.manager.name,V.manager.passwd);
-    LF.OpenRequestFlatRate(V.FRId);
-    LF.FlatRateEditRequest_AddTwoOption();
+    LF.CreateFlatRateFromBoard(V.client);
+    MF.EditRequest_SetAdressToFrom();
+    MF.EditRequest_SendFlatRateSumm(4000);
+    MF.EditRequest_SetToNotConfirmed();
+    JS.step(JSstep.selectTruck(4));
+    MF.WaitWhileBusy();
+    driver.wait(driver.executeScript('return $("input#reserv_price").val()').then(function(text){
+        V.ReservationOnDashBoard = SF.cleanPrice(text);
+    }),config.timeout);
+    V.boardNumbersFlatRate = {};
+    LF.RememberDigitsRequestBoard(V.boardNumbersFlatRate);
     MF.EditRequest_OpenClient();
     LF.SetClientPasswd(V.client.passwd);
     MF.EditRequest_OpenRequest();
-    driver.wait(driver.findElement(By.xpath('//input[@ng-change="changeReservationManually();"]')).getText().then(function (text) {
-        V.ReservationinBoard = SF.cleanPrice(text);
-    }),config.timeout);
-    MF.WaitWhileBusy();
-    V.boardNumbers= {};
-    LF.RememberDigitsRequestBoard(V.boardNumbers);
+    MF.EditRequest_SaveChanges();
     MF.EditRequest_CloseEditRequest();
     MF.Board_LogoutAdmin();
 
-    condition.nowWeDoing = 'переходим на аккаунт, сравниваем числа, выбираем опцию';
+    condition.nowWeDoing = 'идем на аккаунт проверять сумму резервации и конфермить работу';
     SF.get(V.accountURL);
     LF.LoginToAccountAsClient(V.client);
-    MF.Account_OpenRequest(V.boardNumbers.Id);
-    MF.Account_ChooseOptionFlatRate();
-    V.accountNumbersFlatRate = {};
-    LF.(V.accountNumbersFlatRate);
-    LF.Validation_Compare_Account_Admin(V.accountNumbersFlatRate, V.boardNumbers);
-    LF.LogoutFromAccount ();
-    SF.get(V.adminURL);
+    MF.Account_OpenRequest(V.boardNumbersFlatRate.Id);
+    MF.Account_ClickViewRequest();
+    MF.Account_ClickProceedBookYourMove();
+    driver.wait(driver.findElement(By.xpath('//div[@ng-class="{\'disabled\':vm.admin}"]/div/h2[contains(text(),"Deposit: ")]')).getText().then(function (text) {
+        text = SF.cleanPrice(text);
+        VD.IWant(VD.ToEqual, text, V.ReservationOnDashBoard, 'резервация не совпала с той суммой, что указана на мувборде и на конфирмейшн пэйдж');
+    }),config.timeout);
+    MF.Account_ClickIAgreeWithAll();
+    SF.click(By.xpath('//div[@ng-click="addReservationPayment()"]'));
+    SF.waitForVisible(By.xpath('//canvas[@id="signatureCanvasReserv"]'));
+    LF.MakeSignJS('signatureCanvasReserv');
+    SF.click(By.xpath('//button[contains(@ng-click,"saveReservSignature()")]'));
+    LF.FillCardPayModal();
+    MF.WaitWhileSpinner();
+    MF.Account_WaitForGreenTextAfterConfirm();
+    LF.LogoutFromAccount();
 
-    condition.nowWeDoing = 'пошли в админку 2 раз, ставить трак, нот конферм';
-    LF.LoginToBoardAsCustom(V.adminLogin,V.adminPassword);
-    MF.Board_OpenRequest (V.FRId);
-    V.boardNumbers = {};
-    LF.RememberDigitsRequestBoard (V.boardNumbers);
-    LF.FlatRateEditRequest_SendDeliveryDates();
-    JS.step(JSstep.selectTruck((V.boardNumbers.LaborTimeMax + V.boardNumbers.TravelTime)/60));
+
+    condition.nowWeDoing = 'выходим из аккаунта, переходим на мувборд делаем клон нашего реквеста';
+    SF.get (V.adminURL);
+    LF.LoginToBoardAsCustom(V.manager.name,V.manager.passwd);
+    MF.Board_OpenConfirmed();
+    MF.Board_OpenRequest(V.boardNumbersFlatRate.Id);
+    MF.EditRequest_OpenSettings();
+    MF.EditRequest_ClickCloneRequest();
+    MF.EditRequest_WaitForVisibleCloneRequest();
+    driver.wait(driver.findElement(By.xpath('//div[contains(@class,"requestModal status_1")]//a[@ng-click="select(tabs[0])"]')).getText().then(function(text){
+        V.IdClone = SF.cleanPrice(text);
+    }),config.timeout);
+    SF.click(By.xpath('//div[contains(@class,"requestModal status_1")]//button[@ng-click="cancel()"]'));
+    SF.sleep (2);
+    LF.closeEditRequest();
+    MF.Board_OpenPendingRequest();
+    MF.Board_OpenRequest(V.IdClone);
+    MF.EditRequest_OpenClient();
+    //здесь сравниваем  данные клиента в исходном реквесте и в склонированном
+    driver.wait(driver.findElement(By.xpath('//input[@ng-model="client.field_user_first_name"]')).getAttribute('value').then(function (text) {
+        VD.IWant(VD.ToEqual, text, V.client.name, 'не совпало имя клиента с начальным реквестом, а должно');
+    }),config.timeout);
+    driver.wait(driver.findElement(By.xpath('//input[@ng-model="client.field_user_last_name"]')).getAttribute('value').then(function (text) {
+        VD.IWant(VD.ToEqual, text, V.client.fam , 'не совпало имя клиента с начальным реквестом, а должно');
+    }),config.timeout);
+    driver.wait(driver.findElement(By.xpath('//input[@ng-model="client.field_primary_phone"]')).getAttribute('value').then(function (text) {
+        text = SF.cleanPrice(text);
+        VD.IWant(VD.ToEqual, text, '-'+V.client.phone , 'не совпало имя клиента с начальным реквестом, а должно');
+    }),config.timeout);
+    LF.SetClientPasswd(V.client.passwd);
+    MF.EditRequest_OpenRequest();
+    MF.EditRequest_SetAdressToFrom();
+    MF.EditRequest_SendFlatRateSumm(4000);
+    MF.EditRequest_SetToNotConfirmed();
+    JS.step(JSstep.selectTruck(4));
     MF.WaitWhileBusy();
-    VD.IWant (VD.ToEqual, 4500, V.boardNumbers.Total, 'тотал не совпал с выбранной суммой');
-    MF.EditRequest_SetToNotConfirmed ();
-    SF.click (By.xpath('//button[@ng-click="DeliveryDay()"]'));
-    MF.WaitWhileBusy();
+    SF.click(By.xpath('//input[@ng-model="request.reservation_rate.value"]'));
+    SF.send(By.xpath('//input[@ng-model="request.reservation_rate.value"]'),350);
+    driver.wait(driver.executeScript('return $("input#reserv_price").val()').then(function(text){
+        V.CloneReservation = SF.cleanPrice(text);
+    }),config.timeout);
     MF.EditRequest_SaveChanges();
+    MF.EditRequest_CloseEditRequest();
+    MF.Board_LogoutAdmin();
+    condition.nowWeDoing = 'выходим из мувборда, заходим на аккаунт, октрываем клонированный реквест,переходим на конфирмейшн' +
+        'сравниваем резервации в склонированном реквесте на мувборде и на аккаунте';
+    SF.get(V.accountURL);
+    LF.LoginToAccountAsClient(V.client);
+    MF.Account_OpenRequest(V.IdClone);
+    MF.Account_ClickProceedBookYourMove();
+    driver.wait(driver.findElement(By.xpath('//div[@ng-class="{\'disabled\':vm.admin}"]/div/h2[contains(text(),"Deposit: ")]')).getText().then(function (text) {
+        text = SF.cleanPrice(text);
+        VD.IWant(VD.ToEqual, text, V.CloneReservation, 'резервация не совпала с той суммой, что указана на мувборде в склонированном реквесте и на конфирмейшн пэйдж');
+    }),config.timeout);
+    MF.Account_ClickIAgreeWithAll();
+    SF.click(By.xpath('//div[@ng-click="addReservationPayment()"]'));
+    SF.waitForVisible(By.xpath('//canvas[@id="signatureCanvasReserv"]'));
+    LF.MakeSignJS('signatureCanvasReserv');
+    SF.click(By.xpath('//button[contains(@ng-click,"saveReservSignature()")]'));
+    LF.FillCardPayModal();
+    MF.WaitWhileSpinner();
+    MF.Account_WaitForGreenTextAfterConfirm();
+    LF.LogoutFromAccount();
 
+    condition.nowWeDoing = 'выходим из аккаунта, переходим на мувборд, делаем проверку что наш реквест в статусе конферм' +
+        'и проверяем оплату в пэйментах';
+    SF.get(V.adminURL);
+    LF.LoginToBoardAsCustom(V.manager.name,V.manager.passwd);
+    MF.Board_OpenConfirmed();
+    MF.Board_OpenRequest(V.IdClone);
+    JS.scroll('label[ng-click="OpenPaymentModal();"]');
+    SF.click(By.xpath('//label[@ng-click="OpenPaymentModal();"]'));
+    SF.sleep(1);
+    driver.wait(driver.findElement(By.xpath('//tr[@ng-click="prepareToDelete($index, receipt.id)"]/td[4]')).getText().then(function (text) {
+        text = SF.cleanPrice(text);
+        VD.IWant(VD.ToEqual, text, V.CloneReservation, 'резервация не совпала с той суммой, что указана на мувборде в склонированном реквесте и на конфирмейшн пэйдж');
+    }),config.timeout);
 
-
-
-
-
+    SF.endOfTest();
 };
