39381b713f63b61cd9089bea91a8f309eeee069b
stokratt@gmail.com <stokratt@gmail.com>
2018-08-07 11:28:17 +0300

    fix review

--- Node/configs/progress.js
+++ Node/configs/progress.js
@@ -1,6 +1,6 @@
 module.exports = function(config,V) {
 
-    config.suite = ['./tests/92_PackingDay.js'];
+    config.suite = ['./tests/79_ExtraFeeForAdditionalPickUp.js'];
     config.chainFail = false ;
     config.browser = 'chrome';
     config.timeout = 30000;
