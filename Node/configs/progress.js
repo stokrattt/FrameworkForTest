3ef4ffbe0318020795a9453cfedd1e3dd7acab1f
stokratt <stokratt@gmail.com>
2018-08-13 16:34:20 +0300

    add sleep

--- Node/configs/progress.js
+++ Node/configs/progress.js
@@ -1,6 +1,6 @@
 module.exports = function(config,V) {
 
-    config.suite = ['./tests/120_LDFlag.js'];
+    config.suite = ['./tests/81_NewPackingAddCrew.js'];
     config.chainFail = false ;
     config.browser = 'chrome';
     config.timeout = 30000;
