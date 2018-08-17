098a639f5e94df77044b59cd7c5959832ae6b209
stokratt <stokratt@gmail.com>
2018-08-17 10:37:27 +0300

    delete sweet confirm

--- Node/configs/progress.js
+++ Node/configs/progress.js
@@ -1,6 +1,6 @@
 module.exports = function(config,V) {
 
-    config.suite = ['./tests/37_PendingStorage.js'];
+    config.suite = ['./tests/122_MovingAndMovStorageCalcOffCheck.js'];
     config.chainFail = false ;
     config.browser = 'chrome';
     config.timeout = 30000;
